9bc7ecc93b30aba0c980bd38e7563c3fa210243d
Glitch (hello-webpage) <none>
2018-10-21 20:42:35 +0000

    💶🚏 Checkpoint ./index.html:391666/1593 ./script.js:391666/288

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-// prints "hi" in the browser's dev tools console
-console.log('hi');
+const version = 6;
+console.log("Hello PWA - Version " + version);
+document.getElementById("version").innerHTML = version;
