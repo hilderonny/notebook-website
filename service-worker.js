b8af0213f112f166d9d84ed9ed274599a28224d8
Glitch (hello-webpage) <none>
2018-10-22 13:11:09 +0000

    🏋😚 Checkpoint ./service-worker.js:391666/474

--- service-worker.js
+++ service-worker.js
@@ -3,9 +3,25 @@ var filesToCache = [];
 
 // Installation
 self.addEventListener('install', function(evt) {
-  console.log('Installation');
+  console.log('Install ' + cacheName);
   evt.waitUntil(caches.open(cacheName).then(function(cache) {
     console.log('Caching app shell');
     return cache.addAll(filesToCache);
   }));
+});
+
+// Activation
+self.addEventListener('activate', function(evt) {
+  console.log('Activate ' + cacheName);
+    evt.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
 });
