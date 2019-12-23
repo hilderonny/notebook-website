d5a0cd1ae77c9afd6929b91d10eeab8b2d64dc14
Glitch (hello-webpage) <none>
2019-12-23 15:29:16 +0000

    🚍😽 Checkpoint ./public/js/book.js:3682841/550 ./public/index.html:3682841/129

--- public/js/book.js
+++ public/js/book.js
@@ -1,1 +1,31 @@
-/* gl
+/* global LocalDb */
+
+async function createbook() {
+  var book = { _id: Date.now(), pageids: [], };
+  var firstpage = { _id: Date.now(), bookid: book._id, };
+  book.pageids.push(firstpage._id);
+  book.currentpage = firstpage._id;
+  await LocalDb.save('books', book);
+  await LocalDb.save('pages', firstpage);
+  return book;
+}
+
+async function savebook(book) {
+  
+}
+
+async function loadbooks() {
+  
+}
+
+async function loadbook(bookid) {
+  
+}
+
+async function savepage(page) {
+  
+}
+
+async function loadpage(pageid) {
+  
+}
